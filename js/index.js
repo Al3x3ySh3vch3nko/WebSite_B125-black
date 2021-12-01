b9ca23594f576fedd6d5447be24800aa32140902
Al3x3ySh3vch3nko <a.r.shevchenko@gmail.com>
2021-12-01 17:11:33 +0300

    +

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(function()
+{
+    $('.slider__inner, .news__slider-inner').slick
+    ({
+        nextArrow: '<button type="button" class="slider-arrow slick-next icon-angle-right"></button>',
+        prevArrow: '<button type="button" class="slider-arrow slick-prev icon-angle-left"></button>'
+    })
+});
